0f7edecf2746360c66c821e782053f9b20a451d9
Angus Hollands <goosey15@gmail.com>
2025-08-15 15:38:41 +0100

    feat: add to menu

--- src/index.ts
+++ src/index.ts
@@ -4,33 +4,42 @@ import {
 } from '@jupyterlab/application';
 import { INotebookTracker } from '@jupyterlab/notebook';
 import { ICommandPalette } from '@jupyterlab/apputils';
+import { ITranslator, nullTranslator } from '@jupyterlab/translation';
 /**
- * Initialization data for the juptyerlab-stateless-run extension.
+ * Initialization data for the jupyterlab-stateless-run extension.
  */
 const plugin: JupyterFrontEndPlugin<void> = {
-  id: 'juptyerlab-stateless-run:plugin',
+  id: 'jupyterlab-stateless-run:plugin',
   description: 'A JupyterLab extension.',
   autoStart: true,
   requires: [ICommandPalette, INotebookTracker],
+  optional: [ITranslator],
   activate: (
     app: JupyterFrontEnd,
     palette: ICommandPalette,
-    tracker: INotebookTracker
+    tracker: INotebookTracker,
+    translator: ITranslator | null
   ) => {
+    const trans = (translator ?? nullTranslator).load('jupyterlab');
     const { commands, shell } = app;
-    const command = 'paul:clear-run-to-selected';
+    const command = 'stateless:clear-restart-run-to-selected';
 
-    console.log('JupyterLab extension juptyerlab-stateless-run is activated!');
+    console.log('JupyterLab extension jupyterlab-stateless-run is activated!');
     // Add a command
+
+    const isEnabled = () => {
+      console.log('enabed', { tracker, shell });
+      return (
+        tracker.currentWidget !== null &&
+        tracker.currentWidget === shell.currentWidget
+      );
+    };
+
     commands.addCommand(command, {
-      label: 'Clear All Outputs, Restart Kernel, and Run to Selected',
-      caption: 'Clear all outputs, restart kernel, and run to selected',
-      isEnabled: () => {
-        return (
-          tracker.currentWidget !== null &&
-          tracker.currentWidget === shell.currentWidget
-        );
-      },
+      label: 'Restart Kernel, Clear Outputs, and Run All Above Selected Cell',
+      caption:
+        'Clear all outputs, restart kernel, and run all cells above the selected cell',
+      isEnabled,
       execute: async (args: any) => {
         const orig = args['origin'];
         console.log(`${command} has been called from... ${orig}.`);
@@ -56,7 +65,7 @@ const plugin: JupyterFrontEndPlugin<void> = {
     });
 
     // Add the command to the command palette
-    const category = 'Extension Examples';
+    const category = trans.__('Notebook Operations');
     palette.addItem({ command, category, args: { origin: 'from palette' } });
   }
 };
