e097456263bb581eec72131365df5c1b1645410d
Angus Hollands <goosey15@gmail.com>
2025-09-19 17:21:59 +0100

    feat: add reload plugin

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,14 @@ import { INotebookTracker } from '@jupyterlab/notebook';
 import { ICommandPalette } from '@jupyterlab/apputils';
 import { ITranslator, nullTranslator } from '@jupyterlab/translation';
 
+const RESTART_RUN_STATELESS = "gennaker-tools:restart-run-stateless";
+const RESET_JUPYTERLAB = "gennaker-tools:reset-jupyterlab";    
+
 /**
  * Initialization data for the gennaker-tools extension.
  */
-const plugin: JupyterFrontEndPlugin<void> = {
-  id: 'gennaker-tools:plugin',
+export const statelessRunPlugin: JupyterFrontEndPlugin<void> = {
+  id: 'gennaker-tools:stateless-run',
   description: 'A JupyterLab extension.',
   autoStart: true,
   requires: [ICommandPalette, INotebookTracker],
@@ -23,7 +26,6 @@ const plugin: JupyterFrontEndPlugin<void> = {
   ) => {
     const trans = (translator ?? nullTranslator).load('jupyterlab');
     const { commands, shell } = app;
-    const command = 'stateless:clear-restart-run-to-selected';
 
     console.log('JupyterLab extension gennaker-tools is activated!');
     // Add a command
@@ -36,17 +38,16 @@ const plugin: JupyterFrontEndPlugin<void> = {
       );
     };
 
-    commands.addCommand(command, {
+    commands.addCommand(RESTART_RUN_STATELESS, {
       label: 'Restart Kernel, Clear Outputs, and Run All Above Selected Cell',
       caption:
         'Clear all outputs, restart kernel, and run all cells above the selected cell',
       isEnabled,
       execute: async (args: any) => {
         const orig = args['origin'];
-        console.log(`${command} has been called from... ${orig}.`);
+        console.log(`${RESTART_RUN_STATELESS} has been called from... ${orig}.`);
         if (orig !== 'init') {
           // Clear all outputs
-
           await commands.execute('apputils:run-all-enabled', {
             commands: [
               'notebook:clear-all-cell-outputs',
@@ -59,8 +60,44 @@ const plugin: JupyterFrontEndPlugin<void> = {
 
     // Add the command to the command palette
     const category = trans.__('Notebook Operations');
-    palette.addItem({ command, category, args: { origin: 'from palette' } });
+    palette.addItem({ command: RESTART_RUN_STATELESS, category, args: { origin: 'from palette' } });
   }
 };
 
-export default plugin;
+
+/**
+ * Initialization data for the gennaker-tools extension.
+ */
+export const reloadPlugin: JupyterFrontEndPlugin<void> = {
+  id: 'gennaker-tools:reload',
+  description: 'A JupyterLab extension.',
+  autoStart: true,
+  requires: [ICommandPalette],
+  optional: [ITranslator],
+  activate: (
+    app: JupyterFrontEnd,
+    palette: ICommandPalette,
+    translator: ITranslator | null
+  ) => {
+      const { commands } = app;
+      const trans = (translator ?? nullTranslator).load('jupyterlab');
+      
+      commands.addCommand(RESET_JUPYTERLAB, {
+          label: 'Reset JupyterLab',
+          caption: 'Reset JupyterLab',
+          isEnabled: () => true,
+          execute: (args: any) => {
+            const orig = args['origin'];
+            if (orig !== 'init') {
+              window.location.reload();
+            }
+          }
+        });
+
+    // Add the command to the command palette
+    const category = trans.__('Reset');
+    palette.addItem({ command: RESET_JUPYTERLAB, category, args: { origin: 'from palette' } });
+  }
+};
+
+export default [ statelessRunPlugin, reloadPlugin ];
