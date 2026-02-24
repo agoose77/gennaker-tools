54d0f00577d9fb9c2a87056e4e0517e68f4ae530
Angus Hollands <goosey15@gmail.com>
2026-02-24 22:57:28 +0000

    chore: fix env on NixOS

--- flake.nix
+++ flake.nix
@@ -1,76 +1,40 @@
-# code-owner: @agoose77
-# This flake sets up an FSH dev-shell that installs all the required
-# packages for running deployer, and then installs the tool in the virtual environment
-# It is not best-practice for the nix-way of distributing this code,
-# but its purpose is to get an environment up and running.
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   };
   outputs = {
     self,
     nixpkgs,
-    flake-utils,
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
-      };
+  }: let
+    forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
+  in {
+    devShells = forAllSystems (system: let
+      pkgs = import nixpkgs {inherit system;};
       inherit (pkgs) lib;
-
-      python = pkgs.python313;
-      node = pkgs.nodejs_24;
-      packages =
-        [
-          python
-          node
-        ]
-        ++ (with pkgs; [
-          cmake
-          ninja
-          gcc
-          pre-commit
-        ]);
-      shellHook = ''
-        # Unset leaky PYTHONPATH
-        unset PYTHONPATH
-
-        __hash=$(echo ${python.interpreter} | sha256sum)
-
-        # Setup if not defined ####
-        if [[ ! -f ".venv/$__hash" ]]; then
-            __setup_env() {
-                # Remove existing venv
-                if [[ -d .venv ]]; then
-                    rm -r .venv
-                fi
-
-                # Stand up new venv
-                ${python.interpreter} -m venv .venv
-
-                ".venv/bin/python" -m pip install -e "."
-
-                # Add a marker that marks this venv as "ready"
-                touch ".venv/$__hash"
-            }
-
-            __setup_env
-        fi
-        ###########################
-
-        # Activate venv
-        source .venv/bin/activate
-      '';
-      env = lib.optionalAttrs pkgs.stdenv.isLinux {
-        # Python uses dynamic loading for certain libraries.
-        # We'll set the linker path instead of patching RPATH
-        LD_LIBRARY_PATH = lib.makeLibraryPath pkgs.pythonManylinuxPackages.manylinux2014;
-      };
+      python = pkgs.python314;
     in {
-      devShell = pkgs.mkShell {
-        inherit env packages shellHook;
-      };
+      default =
+        (pkgs.buildFHSEnv {
+          name = "python-env";
+          targetPkgs = pkgs:
+            (with pkgs; [
+              pythonManylinuxPackages.manylinux2014Package
+              cmake
+              ninja
+              gcc
+              pre-commit
+              nodejs_22
+            ])
+            ++ [python];
+          runScript = "${pkgs.writeShellScriptBin "runScript" ''
+            test -d .venv || ${python.interpreter} -m venv .venv
+            source .venv/bin/activate
+            set +e
+
+            exec ${lib.getExe pkgs.bash} "$@"
+          ''}/bin/runScript";
+        })
+        .env;
     });
+  };
 }
