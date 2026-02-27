b005f85d752cf7afd29a48129e16fa19c3a1dd04
Angus Hollands <goosey15@gmail.com>
2026-02-27 13:28:56 +0000

    fix: update flake (#28)

--- flake.nix
+++ flake.nix
@@ -6,10 +6,15 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    dev-python = {
+      url = "github:agoose77/dev-flakes/e0b0b96?dir=python";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
   outputs = {
     self,
     nixpkgs,
+    dev-python,
   }: let
     forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
   in {
@@ -21,12 +26,15 @@
       inherit (pkgs) lib;
 
       python = pkgs.python313;
+      venvHook =
+        dev-python.packages.${system}.nix-ld-venv-hook.override
+        {python = python;};
       manyLinux = pkgs.pythonManylinuxPackages.manylinux2014;
       node = pkgs.nodejs_24;
       packages =
         [
           python
-          python.pkgs.venvShellHook
+          venvHook
           node
         ]
         ++ (with pkgs; [
@@ -43,8 +51,6 @@
     in {
       default = pkgs.mkShell {
         inherit packages;
-        # Define additional input for patching interpreter
-        nativeBuildInputs = [pkgs.makeWrapper];
 
         venvDir = ".venv";
 
@@ -53,20 +59,10 @@
 
         # Setup venv by patching interpreter with LD_LIBRARY_PATH
         # This is required because ld does not exist on Nix systems
-        postVenvCreation = let
-          # Find the interpreter of the venv
-          interpreterSubPath = lib.path.subpath.join ["bin" (baseNameOf python.interpreter)];
-        in
-          unwantedEnvPreamble
-          # Patch the venv to find the dynamic libs
-          + ''
-            wrapProgram "$VIRTUAL_ENV/${interpreterSubPath}" --prefix "LD_LIBRARY_PATH" : "${lib.makeLibraryPath manyLinux}"
-          ''
-          +
-          # Install package
-          ''
-            pip install -e ".[dev]"
-          '';
+        postVenvCreation = ''
+          ${unwantedEnvPreamble}
+          pip install -e ".[dev]"
+        '';
       };
     });
   };
