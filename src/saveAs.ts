683fd28975c204a14dfea71e55d4f45b60fff61e
Grace G Pascarella <pascargr@bc.edu>
2026-06-08 13:30:20 -0400

    work for implementing save as function

--- src/saveAs.ts
+++ src/saveAs.ts
@@ -7,6 +7,7 @@ import { ITranslator, nullTranslator } from '@jupyterlab/translation';
 import { Widget } from '@lumino/widgets';
 import { IDocumentManager } from '@jupyterlab/docmanager';
 import { IContentsManager } from '@jupyterlab/services';
+import type { Contents } from '@jupyterlab/services';
 /**
  * Initialization data for the gennaker-tools extension.
  */
@@ -43,7 +44,7 @@ export const saveAsPlugin: JupyterFrontEndPlugin<void> = {
   activate: (
     app: JupyterFrontEnd,
     docManager: IDocumentManager,
-    contents: IContentsManager,
+    contents: Contents.IManager,
     translator: ITranslator | null
   ) => {
     const trans = (translator ?? nullTranslator).load('jupyterlab');
@@ -55,7 +56,7 @@ export const saveAsPlugin: JupyterFrontEndPlugin<void> = {
     // TODO: move to another location
         commands.addCommand('gennaker-tools:save-as', {
         label: () =>
-        "Save as",
+        "Save As",
         caption: 'Save with new path',
         isEnabled: () => true,
         describedBy: {
@@ -64,21 +65,114 @@ export const saveAsPlugin: JupyterFrontEndPlugin<void> = {
             properties: {}
         }
         },
-        execute: () => {
-            const context = docManager.contextForWidget(shell.currentWidget!);
-            const localPath = docManager.contents.localPath(context.path);
-            console.log("testing new save as")
-            const saveBtn = Dialog.okButton({ label: trans.__('Save'), accept: true });
-            return showDialog({
+        execute: async () => {
+
+          // get context of current file
+          const context = docManager.contextForWidget(shell.currentWidget!);
+          if (context === undefined) {
+            return;
+          }
+
+          // get local path of current file
+          const localPath = context.localPath;
+          const factory = 
+
+
+          // get parent of current file
+          const pathParts = localPath.split('/').slice(0, -1);
+          const directoryPath = pathParts.join('/');
+
+          let destinationPath: string; 
+
+          // check to see if parent is writable
+          const contentsResult = await contents.get(directoryPath);
+          const writable = contentsResult.writable;
+
+          // if writable, save as destination remains the same
+          if (writable) {
+            destinationPath = localPath;
+          }
+
+          // if read only, write destination is the designated folder 
+          // FIXME: can define desired folder in config file
+          else {
+            const fileName = localPath.split('/').pop();
+            destinationPath = [...pathParts.slice(0, -1), "test_folder", fileName].join('/');
+          }
+        
+          console.log("testing new save as")
+          const saveBtn = Dialog.okButton({ label: trans.__('Save'), accept: true });
+
+          const savePath = await showDialog({
             title: trans.__('Save File As…'),
-            body: new SaveWidget("test_path"),
+            // prompt save as dialog with destination path (want to add custom message)
+            body: new SaveWidget(destinationPath),
             buttons: [Dialog.cancelButton(), saveBtn]
             }).then(result => {
             if (result.button.accept) {
                 return result.value ?? undefined;
             }
             return;
+          });
+
+          if (savePath === undefined) {
+            return;
+          }
+
+          const drive = contents.driveName(context.path);
+          const newPath = drive == '' ? savePath : `${drive}:${savePath}`;
+
+          if (newPath === context.path) {
+            await context.save();
+            return;
+          }
+
+          const maybeOverWrite = async (path: string) => {
+            const body = trans.__(
+              '"%1" already exists. Do you want to replace it?',
+              path
+            );
+            const overwriteBtn = Dialog.warnButton({
+              label: trans.__('Overwrite'),
+              accept: true
             });
+            return showDialog({
+              title: trans.__('File Overwrite?'),
+              body,
+              buttons: [Dialog.cancelButton(), overwriteBtn]
+            }).then(result => {
+              if (result.button.accept) {
+                return contents.delete(path).then(() => {
+                  const model = context.model;
+                  const options = (
+                    
+                  )
+                  return contents.save(path, options);
+                });
+              } else {
+                return Promise.reject(new Error('Cancelled'));
+              }
+            });
+          }
+
+          // Make sure the path does not exist.
+          try {
+            // await docManager.ready;
+            await contents.get(newPath, {
+              content: false
+            });
+            await maybeOverWrite(newPath);
+          } catch (err) {
+            if (!err.response || err.response.status !== 404) {
+              // Dialog rejection (user cancelled)
+              if (!err.response) {
+                return false;
+              }
+              throw err;
+            }
+          }
+          await this._finishSaveAs(newPath);
+          
         }
     })
   }
